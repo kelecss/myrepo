552d5d52de83fffa84e4dd8cc68551ec3fe43ecb
kelecss <kerim.lengwiler@hotmail.com>
2024-03-23 09:16:43 +0100

    Basic Elements of DS

---
+++ Introduction to DS.R
@@ -0,0 +1,10 @@
+install.packages("tidyverse")
+install.packages("rves")
+library("rvest")
+library("tidyverse")
+url <- "https://www.20min.ch/"
+read_html(url) %>%
+  html_elements(css= "a")  %>%
+  html_attr("href")     
+
+getwd()
