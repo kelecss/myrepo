bb65f0fd45229de474494e0aedb13d25b522d716
kelecss <162478502+kelecss@users.noreply.github.com>
2024-03-27 10:49:27 +0100

    Update Testing file storage.R
    describing how to add a script to a specific folder on GitHub

--- docs/Testing file storage.R
+++ docs/Testing file storage.R
@@ -1,2 +1,5 @@
 getwd()
 #Another file to figure out where my files are stored on github
+#File was finally stored in docs/
+#Way to go: Store script locally in the general project folder. Add it to the specific folder you like the script committed to (e.g. docs/). 
+#Finally push the specific folder you changed (e.g. docs/) onto github
